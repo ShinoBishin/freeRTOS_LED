aca308078f2966e484c867820495ae934f21ae51
kohey0701 <kohey0701@gmail.com>
2021-03-17 19:02:54 +0900

    first commit

---
+++ externed/rtos_selfcheck.ino
@@ -0,0 +1,73 @@
+#include <Arduino_FreeRTOS.h>
+#define LED 2
+
+void TaskBlink(void *pvParameters);
+void TaskChecker(void *pvParameters);
+
+void setup()
+{
+    Serial.begin(9600);
+
+    xTaskCreate(TaskBlink, "Blink", 128, NULL, 2, NULL);
+
+    xTaskCreate(TaskChecker, "Checker", 128, NULL, 1, NULL);
+}
+
+void loop()
+{
+}
+unsigned char led_state;
+
+void TaskBlink(void *pvParameters)
+{
+    (void)pvParameters;
+
+    pinMode(LED, OUTPUT);
+
+    for (;;)
+    {
+        digitalWrite(LED, HIGH);
+        led_state = HIGH;
+        Serial.print("HIGH, ");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        digitalWrite(LED, LOW);
+        led_state = LOW;
+        Serial.print("LOW, ");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+void TaskChecker(void *pvParameters)
+{
+    (void)pvParameters;
+
+    int value = analogRead(A0);
+    int i = 0;
+    for (;;)
+    {
+
+        value = analogRead(A0);
+
+        Serial.println(value);
+        delay(100);
+        i++;
+        if (i == 30)
+        {
+            if ((value < 700) && (led_state == HIGH))
+            {
+                Serial.println(", くらい");
+                i = 0;
+            }
+            else if ((value > 900) && (led_state == HIGH))
+            {
+                Serial.println(", 明るすぎ！");
+                i = 0;
+            }
+            else
+            {
+                i = 0;
+            }
+        }
+        vTaskDelay(1);
+    }
+}
---
+++ rtos_queue_test.ino
@@ -0,0 +1,127 @@
+// セマフォとキューの扱い方を理解する
+
+#include <Arduino_FreeRTOS.h>
+#include <queue.h>
+#include <semphr.h>
+
+TaskHandle_t TaskBlink_Handler;
+TaskHandle_t TaskButton_Handler;
+TaskHandle_t TaskSerial_Handler;
+
+QueueHandle_t InterTaskQueue;
+SemaphoreHandle_t SerialSemaphore;
+
+void TaskBlink(void *pvParameters);
+void TaskButton(void *pvParameters);
+void TaskSerial(void *pvParameters);
+
+void setup()
+{
+    Serial.begin(9600);
+
+    while (!Serial)
+    {
+        ;
+    }
+    if (SerialSemaphore == NULL)
+    {
+        SerialSemaphore = xSemaphoreCreateMutex();
+        if ((SerialSemaphore) != NULL)
+            xSemaphoreGive((SerialSemaphore));
+    }
+
+    InterTaskQueue = xQueueCreate(1, sizeof(int));
+    if (InterTaskQueue != NULL)
+    {
+        xTaskCreate(TaskBlink, "Blink", 128, NULL, 2, &TaskBlink_Handler);
+        xTaskCreate(TaskButton, "Button", 128, NULL, 1, &TaskButton_Handler);
+        xTaskCreate(TaskSerial, "Serial", 128, NULL, 0, &TaskSerial_Handler);
+
+        vTaskStartScheduler();
+    }
+}
+
+void loop()
+{
+    // empty
+}
+
+void TaskSerial(void *pvParameters)
+{
+    (void)pvParameters;
+    for (;;)
+    {
+        while (Serial.available() > 0)
+        {
+            String str = Serial.readString();
+            if (str[0] == 's')
+            {
+                vTaskSuspend(TaskBlink_Handler);
+                Serial.println("Suspend!");
+            }
+            else if (str[0] == 'r')
+            {
+                vTaskResume(TaskBlink_Handler);
+                Serial.println("Resume!");
+            }
+            else
+            {
+                int val = str.toInt();
+                xQueueSend(InterTaskQueue, &val, portMAX_DELAY);
+            }
+            vTaskDelay(1);
+        }
+    }
+}
+
+void TaskButton(void *pvParameters)
+{
+    (void)pvParameters;
+    const int DIN_PIN = 7;
+    int val;
+
+    pinMode(DIN_PIN, INPUT_PULLUP);
+    for (;;)
+    {
+        val = digitalRead(DIN_PIN);
+        if (val == LOW)
+        {
+            if (xSemaphoreTake(SerialSemaphore, (TickType_t)5) == pdTRUE)
+            {
+                Serial.println("Button Pushed");
+                delay(1500);
+                xSemaphoreGive(SerialSemaphore);
+            }
+        }
+        vTaskDelay(1);
+    }
+}
+
+void TaskBlink(void *pvParameters)
+{
+    const int LED_PIN = 13;
+    (void)pvParameters;
+    pinMode(LED_PIN, OUTPUT);
+
+    for (;;)
+    {
+        int count;
+        if (xQueueReceive(InterTaskQueue, &count, portMAX_DELAY) == pdPASS)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (xSemaphoreTake(SerialSemaphore, (TickType_t)5) == pdTRUE)
+                {
+                    char buf[10];
+                    sprintf(buf, "N=%d", i + 1);
+                    Serial.println(buf);
+                    xSemaphoreGive(SerialSemaphore);
+                }
+                digitalWrite(LED_PIN, HIGH);
+                vTaskDelay(500 / portTICK_PERIOD_MS);
+                digitalWrite(LED_PIN, LOW);
+                vTaskDelay(500 / portTICK_PERIOD_MS);
+            }
+        }
+    }
+}
